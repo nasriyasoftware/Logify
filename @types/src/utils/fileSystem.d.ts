253622a05239d1fe0b7d4163b07e7aeeffb6ca7e
nasriyasoftware <nasriya.software@outlook.com>
2024-05-13 17:02:00 +0300

    Removed types

--- @types/src/utils/fileSystem.d.ts
+++
@@ -1,9 +0,0 @@
-import { WriteStream } from 'fs';
-
-declare class FileSystemManager {
-    write(chunk: string, stream: WriteStream): void;
-    get _logFolderLocation(): string;
-    get _logFileLocation(): string;
-}
-
-export default FileSystemManager;
